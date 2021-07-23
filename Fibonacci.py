64948a72867144a87433210b7971055907242cad
freshhoe <yh91211@gmail.com>
2021-07-23 15:28:30 +0900

    feat: Fibonacci by recursion

---
+++ Fibonacci.py
@@ -0,0 +1,9 @@
+# Fibonacci by recursion
+def fib(n):
+    if n == 0:
+        print 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+print(fib(20))
